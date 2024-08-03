07daa4307512911870fb51bc47254fc8d8a05f85
Elie Donadio <elidoset@gmail.com>
2024-08-03 14:12:53 +0200

    use new plugin template

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -19,8 +19,6 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 
 #include <obs-module.h>
 
-#include "plugin-macros.generated.h"
-
 #include <random>
 #include <fstream>
 #include <graphics/image-file.h>
@@ -32,7 +30,8 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 
 using namespace Stinger3D;
 
-class stinger_3D_transition {
+class stinger_3D_transition
+{
 public:
     stinger_3D_transition(obs_data_t *settings, obs_source_t *source);
 
@@ -46,6 +45,8 @@ private:
     gs_eparam_t *bparam;
     gs_eparam_t *tparam;
     std::chrono::milliseconds media_duration;
+    uint32_t resolution_x = 1920;
+    uint32_t resolution_y = 1080;
 
     Transition transition;
 
@@ -64,6 +65,14 @@ private:
 
     obs_properties_t *getProperties();
 
+    void update_resolution()
+    {
+        obs_video_info info;
+        obs_get_video_info(&info);
+        resolution_x = info.base_width;
+        resolution_y = info.base_height;
+    }
+
     static void getDefaults(obs_data_t *);
 
     float delay;
@@ -73,30 +82,35 @@ private:
 
 obs_source_info SOURCE_2;
 
-static float ease(float t, float offset = 0.0f, float amplitude = 4.0f) {
-    float t2 = (t - offset) * amplitude;
+static double ease(float t, float offset = 0.0f, float amplitude = 4.0f)
+{
+    double t2 = (t - offset) * amplitude;
     return -(cos(M_PI * t2) - 1.0) / 2.0; // sinus
-    //return t2 < 0.5 ? 2.0 * t2 * t2 : 1.0 - powf(-2.0 * t2 + 2.0, 2.0) / 2.0; // quadratic
-    //return t2; // linear
+    // return t2 < 0.5 ? 2.0 * t2 * t2 : 1.0 - powf(-2.0 * t2 + 2.0, 2.0) / 2.0; // quadratic
+    // return t2; // linear
 }
 
 #define DISTANCE 1.0
 
-void stinger_3D_transition::transition_callback(gs_texture_t *a, gs_texture_t *b, float t, uint32_t cx, uint32_t cy) {
-    if (t < delay) {
+void stinger_3D_transition::transition_callback(gs_texture_t *a, gs_texture_t *b, float t, uint32_t cx, uint32_t cy)
+{
+    if (t < delay)
+    {
         t = 0;
-    } else {
+    }
+    else
+    {
         t -= delay;
     }
     gs_projection_push();
-    gs_perspective(90, 1280.0 / 720.0, 1.0f / 2097152.0f, 2097152.0f);
+    gs_perspective(90, (float)resolution_x / (float)resolution_y, 1.0f / 2097152.0f, 2097152.0f);
     gs_matrix_push();
-    gs_matrix_scale3f(1.0 * 1280.0 / 720.0, 1.0, 1.0);
+    gs_matrix_scale3f(1.0f * (float)resolution_x / (float)resolution_y, 1.0, 1.0);
 
     transition.transforms->render_frame(t);
 
-    //center image, camera at one unit of distance from the center
-    //gs_matrix_translate3f(-1, -1, -1.0);
+    // center image, camera at one unit of distance from the center
+    // gs_matrix_translate3f(-1, -1, -1.0);
 
     const bool previous = gs_framebuffer_srgb_enabled();
     gs_enable_framebuffer_srgb(true);
@@ -108,49 +122,51 @@ void stinger_3D_transition::transition_callback(gs_texture_t *a, gs_texture_t *b
     while (gs_effect_loop(effect, "Transition"))
         gs_draw_sprite(NULL, 0, 2, 2);
 
-
     gs_enable_framebuffer_srgb(previous);
     gs_matrix_pop();
     gs_projection_pop();
 }
 
-void stinger_3D_transition::render(gs_effect_t *effect) {
+void stinger_3D_transition::render(gs_effect_t *effect)
+{
     obs_transition_video_render(this->source, [](void *data, gs_texture_t *a, gs_texture_t *b, float t, uint32_t cx,
-                                                 uint32_t cy) {
-        static_cast<stinger_3D_transition *>(data)->transition_callback(a, b, t, cx, cy);
-    });
+                                                 uint32_t cy)
+                                { static_cast<stinger_3D_transition *>(data)->transition_callback(a, b, t, cx, cy); });
     gs_matrix_push();
-    //gs_matrix_scale3f(1.0f, 1.0f, 1.0f);
+    // gs_matrix_scale3f(1.0f, 1.0f, 1.0f);
     obs_source_video_render(media_source);
     gs_matrix_pop();
 }
 
-void stinger_3D_transition::update(obs_data_t *settings) {
+void stinger_3D_transition::update(obs_data_t *settings)
+{
     blog(LOG_INFO, "update transition");
 
-    //descriptor parsing
+    // descriptor parsing
     std::string descriptor_filename(obs_data_get_string(settings, "descriptor"));
     std::fstream file;
     file.open(descriptor_filename, std::fstream::in);
 
     std::string descriptor_string(std::istreambuf_iterator<char>{file}, {});
 
-    try {
+    try
+    {
         json::json descriptor_json = json::json::parse(descriptor_string);
         transition = descriptor_json.get<Transition>();
     }
 
-    catch (json::basic_json<>::exception err) {
+    catch (json::basic_json<>::exception err)
+    {
         blog(LOG_ERROR, "Could not parse json parameters : %s", err.what());
     }
 
-
-    delay = (float) obs_data_get_int(settings, "delay") / 1000.0;
-    //Media initialization
+    delay = (float)obs_data_get_int(settings, "delay") / 1000.0f;
+    // Media initialization
 
     auto newPath = std::string(obs_data_get_string(settings, "Video file"));
 
-    if (newPath != videoPath) {
+    if (newPath != videoPath)
+    {
         blog(LOG_INFO, "Refreshing the source...");
         obs_source_release(media_source);
         obs_data_t *media_settings = obs_data_create();
@@ -163,61 +179,71 @@ void stinger_3D_transition::update(obs_data_t *settings) {
 
         videoPath = newPath;
     }
-
-
 }
 
-void stinger_3D_transition::registerInputSource() {
+void stinger_3D_transition::registerInputSource()
+{
     obs_source_info *source_2 = &SOURCE_2;
     source_2->id = "3d_transition";
     source_2->type = OBS_SOURCE_TYPE_TRANSITION;
-    source_2->get_name = [](void *) { return obs_module_text("TransitionName"); };
-    source_2->create = [](obs_data_t *settings, obs_source_t *source) {
+    source_2->get_name = [](void *)
+    { return obs_module_text("TransitionName"); };
+    source_2->create = [](obs_data_t *settings, obs_source_t *source)
+    {
         return static_cast<void *>(new stinger_3D_transition(settings, source));
     };
-    source_2->get_properties = [](void *data) { return static_cast<stinger_3D_transition *>(data)->getProperties(); };
-    source_2->video_render = [](void *data, gs_effect_t *effect) {
+    source_2->get_properties = [](void *data)
+    { return static_cast<stinger_3D_transition *>(data)->getProperties(); };
+    source_2->video_render = [](void *data, gs_effect_t *effect)
+    {
         return static_cast<stinger_3D_transition *>(data)->render(effect);
     };
     source_2->audio_render = [](void *data, uint64_t *ts_out, struct obs_source_audio_mix *audio, uint32_t mixers,
-                                size_t channels, size_t sample_rate) {
+                                size_t channels, size_t sample_rate)
+    {
         return static_cast<stinger_3D_transition *>(data)->render_audio(ts_out, audio, mixers, channels, sample_rate);
     };
-    source_2->update = [](void *data, obs_data_t *settings) {
+    source_2->update = [](void *data, obs_data_t *settings)
+    {
         static_cast<stinger_3D_transition *>(data)->update(settings);
     };
-    source_2->destroy = [](void *data) { delete static_cast<stinger_3D_transition *>(data); };
-    source_2->get_defaults = [](obs_data_t *data) {
+    source_2->destroy = [](void *data)
+    { delete static_cast<stinger_3D_transition *>(data); };
+    source_2->get_defaults = [](obs_data_t *data)
+    {
         stinger_3D_transition::getDefaults(data);
     };
-    source_2->transition_start = [](void *data) { static_cast<stinger_3D_transition *>(data)->startTransition(); };
-    source_2->transition_stop = [](void *data) { static_cast<stinger_3D_transition *>(data)->endTransition(); };
+    source_2->transition_start = [](void *data)
+    { static_cast<stinger_3D_transition *>(data)->startTransition(); };
+    source_2->transition_stop = [](void *data)
+    { static_cast<stinger_3D_transition *>(data)->endTransition(); };
 
     obs_register_source(source_2);
-
 }
 
-bool
-stinger_3D_transition::render_audio(uint64_t *ts_out, obs_source_audio_mix *audio, uint32_t mixers, size_t channels,
-                                    size_t sample_rate) {
-    return obs_transition_audio_render(source, ts_out, audio, mixers, channels, sample_rate,
-                                       [](void *, float t) { return 1 - t; },
-                                       [](void *, float t) { return t; });
+bool stinger_3D_transition::render_audio(uint64_t *ts_out, obs_source_audio_mix *audio, uint32_t mixers, size_t channels,
+                                         size_t sample_rate)
+{
+    return obs_transition_audio_render(source, ts_out, audio, mixers, channels, sample_rate, [](void *, float t)
+                                       { return 1 - t; }, [](void *, float t)
+                                       { return t; });
 }
 
-stinger_3D_transition::stinger_3D_transition(obs_data_t *settings, obs_source_t *source) {
+stinger_3D_transition::stinger_3D_transition(obs_data_t *settings, obs_source_t *source)
+{
     blog(LOG_INFO, "----- Creating Stream Transition...");
     gs_effect_t *effect;
 
     this->source = source;
     char *file = obs_module_file("transition.effect");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         blog(LOG_ERROR, "file not found");
         return;
     }
 
     obs_enter_graphics();
-    char **error_string = (char **) bmalloc(sizeof(char) * 2048);
+    char **error_string = (char **)bmalloc(sizeof(char) * 2048);
     effect = gs_effect_create_from_file(file, error_string);
     obs_leave_graphics();
 
@@ -233,16 +259,18 @@ stinger_3D_transition::stinger_3D_transition(obs_data_t *settings, obs_source_t 
     this->media_source = nullptr;
 
     bfree(error_string);
-
 }
 
-void stinger_3D_transition::startTransition() {
+void stinger_3D_transition::startTransition()
+{
+    update_resolution();
+
     proc_handler_t *ph = obs_source_get_proc_handler(media_source);
     calldata_t cd = {0};
     proc_handler_call(ph, "get_duration", &cd);
     uint64_t duration_ms = calldata_int(&cd, "duration") / 1000000;
     media_duration = std::chrono::milliseconds(duration_ms);
-    obs_transition_enable_fixed(source, true, media_duration.count());
+    obs_transition_enable_fixed(source, true, static_cast<uint32_t>(media_duration.count()));
 
     proc_handler_call(ph, "restart", &cd);
     blog(LOG_INFO, "Video started !");
@@ -250,11 +278,13 @@ void stinger_3D_transition::startTransition() {
     calldata_free(&cd);
 }
 
-void stinger_3D_transition::endTransition() {
+void stinger_3D_transition::endTransition()
+{
     obs_source_remove_active_child(source, media_source);
 }
 
-obs_properties_t *stinger_3D_transition::getProperties() {
+obs_properties_t *stinger_3D_transition::getProperties()
+{
     auto properties = obs_properties_create();
     obs_properties_add_int(properties, "delay", "Delay between audio and animation (ms)", 0, 1000, 1);
     obs_properties_add_path(properties, "Video file", "Path to the video", OBS_PATH_FILE, NULL, NULL);
@@ -263,7 +293,8 @@ obs_properties_t *stinger_3D_transition::getProperties() {
     return properties;
 }
 
-void stinger_3D_transition::getDefaults(obs_data_t *data) {
+void stinger_3D_transition::getDefaults(obs_data_t *data)
+{
     obs_data_set_default_int(data, "delay", 20);
 }
 
@@ -273,7 +304,8 @@ OBS_MODULE_USE_DEFAULT_LOCALE("3D stinger transition", "en-US")
 
 OBS_MODULE_AUTHOR("ElDonad")
 
-bool obs_module_load() {
+bool obs_module_load()
+{
     stinger_3D_transition::registerInputSource();
     return true;
 }
--- src/plugin-types.cpp
+++ src/plugin-types.cpp
@@ -8,121 +8,140 @@
 #include <obs-module.h>
 #include <graphics/axisang.h>
 
-namespace Stinger3D {
+namespace Stinger3D
+{
 
     const vec3 camera_position = vec3{-1, -1, -1};
 
-    float ease(float input, EaseType type, float amplitude, float offset, float start) {
+    float ease(float input, EaseType type, float amplitude, float offset, float start)
+    {
         float t2 = (input - offset) * (amplitude - start);
         float t2_inv = (1 - (input - offset)) * (amplitude - start);
         float val;
-        switch (type) {
-            case LINEAR:
-                return t2 + start;
-            case SINUSOIDAL: {
-                val = -(amplitude - start) * (cos(M_PI * (input - offset)) - 1.0) / 2.0 + start;
-                return val;
-            }
-            case QUADRATIC:
-                return t2 < 0.5 ? 2.0 * t2 * t2 : 1.0 - powf(-2.0 * t2 + 2.0, 2.0) / 2.0;
-            case CONSTANT:
-                return amplitude;
+        switch (type)
+        {
+        case LINEAR:
+            return t2 + start;
+        case SINUSOIDAL:
+        {
+            val = -(amplitude - start) * (cosf(M_PI * (input - offset)) - 1.0f) / 2.0f + start;
+            return val;
+        }
+        case QUADRATIC:
+            return t2 < 0.5f ? 2.0f * t2 * t2 : 1.0f - powf(-2.0f * t2 + 2.0f, 2.0f) / 2.0f;
+        case CONSTANT:
+            return amplitude;
+        default:
+            return 0;
         }
     }
 
-    std::variant<vec3, quat> Transformation::getFrame(float frame) {
-        if (frame < begin_frame || frame > end_frame) {
+    std::variant<vec3, quat> Transformation::getFrame(float frame)
+    {
+        if (frame < begin_frame || frame > end_frame)
+        {
             if (transformation == ROTATION)
                 return std::variant<vec3, quat>(quat{0, 0, 0, 0});
             else if (transformation == SCALE)
                 return std::variant<vec3, quat>(vec3{1, 1, 1});
             else
                 return std::variant<vec3, quat>(vec3{0, 0, 0});
-        } else {
+        }
+        else
+        {
             float advancement = (frame - begin_frame) / (end_frame - begin_frame);
-            if (transformation == TRANSLATION) {
+            if (transformation == TRANSLATION)
+            {
                 return std::variant<vec3, quat>(vec3{
-                        ease(advancement, easing, std::get<vec3>(parameters).x),
-                        ease(advancement, easing, std::get<vec3>(parameters).y),
-                        ease(advancement, easing, std::get<vec3>(parameters).z),
-                });
-            } else if (transformation == SCALE) {
-                return std::variant<vec3, quat>(vec3{
-                        ease(advancement, easing, std::get<vec3>(parameters).x, 0, 1),
-                        ease(advancement, easing, std::get<vec3>(parameters).y, 0, 1),
-                        ease(advancement, easing, std::get<vec3>(parameters).z, 0, 1),
-                });
-            } else {
-                return std::variant<vec3, quat>(quat{
-                        std::get<quat>(parameters).x,
-                        std::get<quat>(parameters).y,
-                        std::get<quat>(parameters).z,
-                        ease(advancement, easing, std::get<quat>(parameters).w)
+                    ease(advancement, easing, std::get<vec3>(parameters).x),
+                    ease(advancement, easing, std::get<vec3>(parameters).y),
+                    ease(advancement, easing, std::get<vec3>(parameters).z),
                 });
             }
-
+            else if (transformation == SCALE)
+            {
+                return std::variant<vec3, quat>(vec3{
+                    ease(advancement, easing, std::get<vec3>(parameters).x, 0, 1),
+                    ease(advancement, easing, std::get<vec3>(parameters).y, 0, 1),
+                    ease(advancement, easing, std::get<vec3>(parameters).z, 0, 1),
+                });
+            }
+            else
+            {
+                return std::variant<vec3, quat>(quat{
+                    std::get<quat>(parameters).x,
+                    std::get<quat>(parameters).y,
+                    std::get<quat>(parameters).z,
+                    ease(advancement, easing, std::get<quat>(parameters).w)});
+            }
         }
     }
 
-    void to_json(json::json &j, const Frame &frame) {
+    void to_json(json::json &j, const Frame &frame)
+    {
         j["position"] = frame.position;
         j["rotation"] = frame.rotation;
         j["scale"] = frame.scale;
     }
 
-    void from_json(const json::json &j, Frame &frame) {
+    void from_json(const json::json &j, Frame &frame)
+    {
         j.at("position").get_to(frame.position);
         j.at("rotation").get_to(frame.rotation);
         j.at("scale").get_to(frame.scale);
     }
 
-    void to_json(json::json &j, const TransformData &data) {
+    void to_json(json::json &j, const TransformData &data)
+    {
         j["transforms"] = data.transforms;
     }
 
-    void from_json(const json::json &j, TransformData &data) {
+    void from_json(const json::json &j, TransformData &data)
+    {
         j.at("transforms").get_to(data.transforms);
     }
 
-    void to_json(json::json &j, const InterpolationData &data) {
+    void to_json(json::json &j, const InterpolationData &data)
+    {
         j["frames"] = data.raw_frames;
         j["resolution"] = data.resolution;
     }
 
-    void from_json(const json::json &j, InterpolationData &data) {
+    void from_json(const json::json &j, InterpolationData &data)
+    {
         j.at("resolution").get_to(data.resolution);
         j.at("frames").get_to(data.raw_frames);
     }
 
-    float interpolate(float lower, float upper, float offset) {
+    float interpolate(float lower, float upper, float offset)
+    {
         return lower + (upper - lower) * offset;
     };
 
-    void InterpolationData::render_frame(float time) {
-        int lowerFramei = floor(raw_frames.size() * time);
-        int upperFramei = ceil(raw_frames.size() * time);
-        float currentFramei = ((float) raw_frames.size()) * time;
-        float coi = currentFramei - (float) lowerFramei;
+    void InterpolationData::render_frame(float time)
+    {
+        int lowerFramei = (int)floor(raw_frames.size() * time);
+        int upperFramei = (int)ceil(raw_frames.size() * time);
+        float currentFramei = ((float)raw_frames.size()) * time;
+        float coi = currentFramei - (float)lowerFramei;
 
         auto &lf = raw_frames[lowerFramei];
         auto &uf = raw_frames[upperFramei];
         auto pos = vec3{
-                interpolate(lf.position.x, uf.position.x, coi),
-                interpolate(lf.position.y, uf.position.y, coi),
-                interpolate(lf.position.z, uf.position.z, coi)
-        };
+            interpolate(lf.position.x, uf.position.x, coi),
+            interpolate(lf.position.y, uf.position.y, coi),
+            interpolate(lf.position.z, uf.position.z, coi)};
         auto rot = quat{
-                interpolate(lf.rotation.x, uf.rotation.x, coi),
-                interpolate(lf.rotation.y, uf.rotation.y, coi),
-                interpolate(lf.rotation.z, uf.rotation.z, coi),
-                interpolate(lf.rotation.w, uf.rotation.w, coi),
+            interpolate(lf.rotation.x, uf.rotation.x, coi),
+            interpolate(lf.rotation.y, uf.rotation.y, coi),
+            interpolate(lf.rotation.z, uf.rotation.z, coi),
+            interpolate(lf.rotation.w, uf.rotation.w, coi),
         };
 
         auto scale = vec3{
-                interpolate(lf.scale.x, uf.scale.x, coi),
-                interpolate(lf.scale.y, uf.scale.y, coi),
-                interpolate(lf.scale.z, uf.scale.z, coi)
-        };
+            interpolate(lf.scale.x, uf.scale.x, coi),
+            interpolate(lf.scale.y, uf.scale.y, coi),
+            interpolate(lf.scale.z, uf.scale.z, coi)};
 
         gs_matrix_translate(&camera_position);
         gs_matrix_translate(&pos);
@@ -131,107 +150,126 @@ namespace Stinger3D {
         gs_matrix_rotquat(&rot);
         gs_matrix_translate3f(-1, -1, 0);
 
-
         gs_matrix_scale(&scale);
-
     }
 
-    void TransformData::render_frame(float time) {
-        for (auto transform: transforms) {
-            switch (transform.transformation) {
-                case Stinger3D::TRANSLATION: {
-                    auto machin = std::get<vec3>(transform.getFrame(time));
-                    gs_matrix_translate(&machin);
-                    break;
-                }
-                case Stinger3D::ROTATION: {
-                    auto qu = std::get<quat>(transform.getFrame(time));
-                    gs_matrix_rotaa4f(qu.x, qu.y, qu.z, qu.w);
-                    break;
-                }
-                case Stinger3D::SCALE: {
-                    auto val = std::get<vec3>(transform.getFrame(time));
-                    gs_matrix_scale(&val);
-                    break;
-                }
+    void TransformData::render_frame(float time)
+    {
+        for (auto transform : transforms)
+        {
+            switch (transform.transformation)
+            {
+            case Stinger3D::TRANSLATION:
+            {
+                auto machin = std::get<vec3>(transform.getFrame(time));
+                gs_matrix_translate(&machin);
+                break;
+            }
+            case Stinger3D::ROTATION:
+            {
+                auto qu = std::get<quat>(transform.getFrame(time));
+                gs_matrix_rotaa4f(qu.x, qu.y, qu.z, qu.w);
+                break;
+            }
+            case Stinger3D::SCALE:
+            {
+                auto val = std::get<vec3>(transform.getFrame(time));
+                gs_matrix_scale(&val);
+                break;
+            }
             }
         }
     }
 
-    void to_json(json::json &j, const Transformation &transform) {
+    void to_json(json::json &j, const Transformation &transform)
+    {
         j["begin_frame"] = transform.begin_frame;
         j["end_frame"] = transform.end_frame;
         j["easing"] = transform.easing;
         j["transformation"] = transform.transformation;
-        if (transform.transformation == ROTATION) {
+        if (transform.transformation == ROTATION)
+        {
             j["params"] = std::get<quat>(transform.parameters);
-        } else {
+        }
+        else
+        {
             j["params"] = std::get<vec3>(transform.parameters);
         }
     }
 
-    void from_json(const json::json &j, Transformation &transform) {
+    void from_json(const json::json &j, Transformation &transform)
+    {
         j.at("begin_frame").get_to(transform.begin_frame);
         j.at("end_frame").get_to(transform.end_frame);
         j.at("easing").get_to(transform.easing);
         j.at("transformation").get_to(transform.transformation);
-        if (transform.transformation == ROTATION) {
+        if (transform.transformation == ROTATION)
+        {
             transform.parameters = j.at("params").get<quat>();
-        } else {
+        }
+        else
+        {
             transform.parameters = j.at("params").get<vec3>();
         }
     }
 
-    void to_json(json::json &j, const Transition &transition) {
+    void to_json(json::json &j, const Transition &transition)
+    {
         j["swap_time"] = transition.swap_time;
         j["data_type"] = transition.data_type;
-        switch (transition.data_type) {
-            case TRANSFORM:
-                j["data"] = *static_cast<TransformData *>(transition.transforms.get());
-            case INTERPOLATION:
-                j["data"] = *static_cast<InterpolationData *>(transition.transforms.get());
+        switch (transition.data_type)
+        {
+        case TRANSFORM:
+            j["data"] = *static_cast<TransformData *>(transition.transforms.get());
+        case INTERPOLATION:
+            j["data"] = *static_cast<InterpolationData *>(transition.transforms.get());
         }
     }
 
-    void from_json(const json::json &j, Transition &transition) {
+    void from_json(const json::json &j, Transition &transition)
+    {
         j.at("swap_time").get_to(transition.swap_time);
 
         j.at("data_type").get_to(transition.data_type);
-        switch (transition.data_type) {
-            case INTERPOLATION:
-                transition.transforms = std::make_unique<InterpolationData>(
-                        std::move(j.at("data").get<InterpolationData>()));
-                break;
-            case TRANSFORM:
-                transition.transforms = std::make_unique<TransformData>(
-                        std::move(j.at("data").get<TransformData>()));
-                break;
-
+        switch (transition.data_type)
+        {
+        case INTERPOLATION:
+            transition.transforms = std::make_unique<InterpolationData>(
+                std::move(j.at("data").get<InterpolationData>()));
+            break;
+        case TRANSFORM:
+            transition.transforms = std::make_unique<TransformData>(
+                std::move(j.at("data").get<TransformData>()));
+            break;
         }
     }
 
 }
 
-void to_json(json::json &j, const vec3 &transform) {
+void to_json(json::json &j, const vec3 &transform)
+{
     j["x"] = transform.x;
     j["y"] = transform.y;
     j["z"] = transform.z;
 }
 
-void from_json(const json::json &j, vec3 &transform) {
+void from_json(const json::json &j, vec3 &transform)
+{
     j.at("x").get_to(transform.x);
     j.at("y").get_to(transform.y);
     j.at("z").get_to(transform.z);
 }
 
-void to_json(json::json &j, const quat &transform) {
+void to_json(json::json &j, const quat &transform)
+{
     j["x"] = transform.x;
     j["y"] = transform.y;
     j["z"] = transform.z;
     j["w"] = transform.w;
 }
 
-void from_json(const json::json &j, quat &transform) {
+void from_json(const json::json &j, quat &transform)
+{
     j.at("x").get_to(transform.x);
     j.at("y").get_to(transform.y);
     j.at("z").get_to(transform.z);
